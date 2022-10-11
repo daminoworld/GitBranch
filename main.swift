e9297f834c7d0bc3d1df9f44f83bda2778f02d6c
daminoworld <ekadls757@gmail.com>
2022-10-12 01:42:52 +0900

    [ADD] echo program added

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  EchoProgram
+//
+//  Created by 김담인 on 2022/10/12.
+//
+
+import Foundation
+
+let input = readLine()?.components(separatedBy: " ")
+
+if let a = input?.first {
+    print(a)
+}
